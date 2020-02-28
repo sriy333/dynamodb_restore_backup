a20c1d31c34bae495594858665eedadb29a60e1e
sree <rangasaiy33@gmail.com>
2020-02-28 10:36:48 +0530

    Update constants.py

--- dynamodb/constants.py
+++ dynamodb/constants.py
@@ -1,5 +1,5 @@
 AWS_CONFIGURE = "aws configure"
-AWS_KEY_ID = null
-AWS_KEY_SECERT_ID = null
+AWS_KEY_ID = "<key value>"
+AWS_KEY_SECERT_ID = "<secret value>"
 AWS_DEFAULT_REGION = "eu-central-1"
 TABLE_NAME = "backuptest"
